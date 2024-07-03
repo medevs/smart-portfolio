d9da5dd132aa66a6b3dc87ee080f09bb928678c0
medevs <oublihi.a@gmail.com>
2024-07-03 13:19:13 +0200

    Add: github-stats page

---
+++ src/app/github-stats/page.tsx
@@ -0,0 +1,21 @@
+import GitHubInsightsCharts from '@/components/GitHubInsightsCharts';
+import GitHubTrends from '@/components/GitHubTrends';
+import React from 'react';
+
+const GitHubStatsPage: React.FC = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-8 text-center text-gray-800 dark:text-gray-200">GitHub Statistics and Insights</h1>
+      
+      <div className="mb-8">
+        <GitHubInsightsCharts />
+      </div>
+      
+      <div>
+        <GitHubTrends />
+      </div>
+    </div>
+  );
+};
+
+export default GitHubStatsPage;
