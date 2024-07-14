9ddb450e663a48fb9154159445204e54dff421b4
ahmed <oublihi.a@gmail.com>
2024-07-14 18:40:33 +0200

    Edite: navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,6 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-white">
       <AIChatButton />
-      <ArtisticAhmedLetters />
     </footer>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,12 +18,12 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 z-50 backdrop-blur-md bg-white bg-opacity-50">
+    <header className="sticky top-0 z-50 backdrop-blur-md">
       <nav className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-2xl font-bold text-gray-900">
-              Ahmed
+            <Link href="/" className="text-3xl font-bold">
+            ⵣ
             </Link>
           </div>
           <div className="hidden md:block">
@@ -32,12 +32,12 @@ export default function Navbar() {
                 <Link
                   key={link.name}
                   href={link.href}
-                  className="relative px-3 py-2 rounded-md text-sm font-medium text-gray-900 hover:text-black transition-colors duration-300 group flex items-center"
+                  className="relative px-3 py-2 rounded-md text-sm font-medium   group flex items-center"
                 >
                   <link.icon className="w-5 h-5 mr-2" />
                   {link.name}
                   <motion.span
-                    className="absolute bottom-0 left-0 w-full h-0.5 bg-gray-900 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"
+                    className="absolute bottom-0 left-0 w-full h-0.5  transform scale-x-0 group-hover:scale-x-100 "
                     layoutId="underline"
                   />
                 </Link>
