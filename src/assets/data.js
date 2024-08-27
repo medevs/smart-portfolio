20aa3f73b60906b7c8b59f8327b6b2855e1ebf67
medevs <oublihi.a@gmail.com>
2024-08-27 14:02:09 +0200

    cleanup

--- src/assets/data.js
+++
@@ -1,5 +0,0 @@
-export const recentPosts = [
-  { title: "Understanding JavaScript Closures", date: "June 15, 2024" },
-  { title: "Building Responsive Layouts with Tailwind CSS", date: "May 28, 2024" },
-  { title: "Getting Started with TypeScript", date: "April 10, 2024" },
-];
